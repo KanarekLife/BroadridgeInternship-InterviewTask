c1e9a49a25b1cb5f97f0ee5556e3d280449937e6
Stanisław Nieradko <stanislaw@nieradko.com>
2022-05-22 12:26:41 +0200

    update api url

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		apiUrl: 'https://localhost:7210/time/'
+		apiUrl: 'https://localhost:5001/time/'
 	}
 });
 
